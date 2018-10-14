8a280e1d8757e0bf2f1800e67310409092fcceb8
Alexandr Zheleznyakov <delta.agent00@gmail.com>
2018-10-14 18:41:53 +0300

    redisigne news card

--- vk/Views/NewsList/NewsTableViewCell.swift
+++ vk/Views/NewsList/NewsTableViewCell.swift
@@ -13,10 +13,11 @@ class NewsTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameView: UILabel?
     @IBOutlet weak var newsTextView: UILabel?
     @IBOutlet weak var newsImageView: UIImageView?
-    @IBOutlet weak var likeView: UILabel?
-    @IBOutlet weak var commentView: UILabel?
-    @IBOutlet weak var repostView: UILabel?
-    @IBOutlet weak var viewView: UILabel?
+    @IBOutlet weak var likeView: UIButton?
+    @IBOutlet weak var commentView: UIButton?
+    @IBOutlet weak var repostView: UIButton?
+    @IBOutlet weak var viewView: UIButton?
+    @IBOutlet weak var newsImageViewHeightConstraint: NSLayoutConstraint?
     
     func fill(newsFeed: NewsFeed, owner: NewsFeedOwner, cacheImageService: CacheImageService?, indexPath: IndexPath) {
         if let image = cacheImageService?.getPhoto(indexPath: indexPath, url: owner.getUrlImage()) {
@@ -32,21 +33,25 @@ class NewsTableViewCell: UITableViewCell {
             let url = URL(string: items[0].sizes[0].url ?? "")
             newsImageView?.kf.setImage(with: url)
             newsImageView?.isHidden = false
+            newsImageViewHeightConstraint?.constant = 90
         } else {
             newsImageView?.isHidden = true
+            newsImageViewHeightConstraint?.constant = 0
         }
         
         if let likes = newsFeed.likes {
-            likeView?.text = "\(likes.count)"
+            //likeView?.titleLabel?.text = "\(likes.count)"
+            likeView?.setTitle("\(likes.count)", for: .normal)
         }
         if let comments = newsFeed.comments {
-            commentView?.text = "\(comments.count)"
+            commentView?.titleLabel?.text = "\(comments.count)"
         }
         if let reposts = newsFeed.reposts {
-            repostView?.text = "\(reposts.count)"
+            repostView?.titleLabel?.text = "\(reposts.count)"
         }
         if let views = newsFeed.views {
-            viewView?.text = "\(views.count)"
+            //viewView?.titleLabel?.text = "\(views.count)"
+            viewView?.setTitle("\(views.count)", for: .normal)
         }
         
 //        reloadFrames()
@@ -128,47 +133,47 @@ class NewsTableViewCell: UITableViewCell {
     }
     
     func likesFrame() {
-        if let text = likeView?.text, let font = likeView?.font  {
-            let maxWidth = getMaxSocialItemSize()
-            let likesSize = getLabelSize(text: text, font: font, maxWidth: maxWidth)
-            let positionX = MARGIN_LEFT_RIGHT
-            let positionY = (newsImageView?.frame.maxY ?? 0) + MARGIN_TOP_BOTTOM
-            let likesOrigin = CGPoint(x: positionX, y: positionY)
-            likeView?.frame = CGRect(origin: likesOrigin, size: likesSize)
-        }
+//        if let text = likeView?.text, let font = likeView?.font  {
+//            let maxWidth = getMaxSocialItemSize()
+//            let likesSize = getLabelSize(text: text, font: font, maxWidth: maxWidth)
+//            let positionX = MARGIN_LEFT_RIGHT
+//            let positionY = (newsImageView?.frame.maxY ?? 0) + MARGIN_TOP_BOTTOM
+//            let likesOrigin = CGPoint(x: positionX, y: positionY)
+//            likeView?.frame = CGRect(origin: likesOrigin, size: likesSize)
+//        }
     }
     
     func commentsFrame() {
-        if let text = commentView?.text, let font = commentView?.font  {
-            let maxWidth = getMaxSocialItemSize()
-            let likesSize = getLabelSize(text: text, font: font, maxWidth: maxWidth)
-            let positionX = (likeView?.frame.maxX ?? 0) + DIVIDED_SOCIAL_ITEM
-            let positionY = (newsImageView?.frame.maxY ?? 0) + MARGIN_TOP_BOTTOM
-            let likesOrigin = CGPoint(x: positionX, y: positionY)
-            commentView?.frame = CGRect(origin: likesOrigin, size: likesSize)
-        }
+//        if let text = commentView?.text, let font = commentView?.font  {
+//            let maxWidth = getMaxSocialItemSize()
+//            let likesSize = getLabelSize(text: text, font: font, maxWidth: maxWidth)
+//            let positionX = (likeView?.frame.maxX ?? 0) + DIVIDED_SOCIAL_ITEM
+//            let positionY = (newsImageView?.frame.maxY ?? 0) + MARGIN_TOP_BOTTOM
+//            let likesOrigin = CGPoint(x: positionX, y: positionY)
+//            commentView?.frame = CGRect(origin: likesOrigin, size: likesSize)
+//        }
     }
     
     func repostsFrame() {
-        if let text = repostView?.text, let font = repostView?.font  {
-            let maxWidth = getMaxSocialItemSize()
-            let likesSize = getLabelSize(text: text, font: font, maxWidth: maxWidth)
-            let positionX = (commentView?.frame.maxX ?? 0) + DIVIDED_SOCIAL_ITEM
-            let positionY = (newsImageView?.frame.maxY ?? 0) + MARGIN_TOP_BOTTOM
-            let likesOrigin = CGPoint(x: positionX, y: positionY)
-            repostView?.frame = CGRect(origin: likesOrigin, size: likesSize)
-        }
+//        if let text = repostView?.text, let font = repostView?.font  {
+//            let maxWidth = getMaxSocialItemSize()
+//            let likesSize = getLabelSize(text: text, font: font, maxWidth: maxWidth)
+//            let positionX = (commentView?.frame.maxX ?? 0) + DIVIDED_SOCIAL_ITEM
+//            let positionY = (newsImageView?.frame.maxY ?? 0) + MARGIN_TOP_BOTTOM
+//            let likesOrigin = CGPoint(x: positionX, y: positionY)
+//            repostView?.frame = CGRect(origin: likesOrigin, size: likesSize)
+//        }
     }
     
     func viewsFrame() {
-        if let text = viewView?.text, let font = viewView?.font  {
-            let maxWidth = getMaxSocialItemSize()
-            let likesSize = getLabelSize(text: text, font: font, maxWidth: maxWidth)
-            let positionX = (repostView?.frame.maxX ?? 0) + DIVIDED_SOCIAL_ITEM
-            let positionY = (newsImageView?.frame.maxY ?? 0) + MARGIN_TOP_BOTTOM
-            let likesOrigin = CGPoint(x: positionX, y: positionY)
-            viewView?.frame = CGRect(origin: likesOrigin, size: likesSize)
-        }
+//        if let text = viewView?.text, let font = viewView?.font  {
+//            let maxWidth = getMaxSocialItemSize()
+//            let likesSize = getLabelSize(text: text, font: font, maxWidth: maxWidth)
+//            let positionX = (repostView?.frame.maxX ?? 0) + DIVIDED_SOCIAL_ITEM
+//            let positionY = (newsImageView?.frame.maxY ?? 0) + MARGIN_TOP_BOTTOM
+//            let likesOrigin = CGPoint(x: positionX, y: positionY)
+//            viewView?.frame = CGRect(origin: likesOrigin, size: likesSize)
+//        }
     }
     
     func reloadFrames() {
